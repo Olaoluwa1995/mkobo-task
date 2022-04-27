b544b160d34489c69c0e3d04b4408699a6837340
StephLak <olakulehinstephen@gmail.com>
2022-04-27 09:10:09 +0100

    Create app-colors constant

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0XFF0047CC);
+  static const Color secondaryColor = Color.fromRGBO(38, 50, 56, 0.5);
+}
