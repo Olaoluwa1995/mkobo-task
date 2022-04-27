9a15d7ea2bb51ef9eeb50ebc824e7481f7450e89
StephLak <olakulehinstephen@gmail.com>
2022-04-27 12:32:41 +0100

    Added icons to assets

--- lib/constants/app_icons.dart
+++ lib/constants/app_icons.dart
@@ -5,5 +5,13 @@ class AppIcons {
   static const String naira = 'assets/icons/naira.png';
   static const String avatar = 'assets/icons/avatar.png';
   static const String arrow = 'assets/icons/arrow.png';
+  static const String save = 'assets/icons/save.png';
+  static const String transfer = 'assets/icons/transfer.png';
+  static const String receive = 'assets/icons/receive.png';
+  static const String send = 'assets/icons/send.png';
+  static const String netflix = 'assets/icons/netflix.png';
+  static const String payBills = 'assets/icons/pay_bills.png';
+  static const String earlyPay = 'assets/icons/early_pay.png';
+  static const String buyAirtime = 'assets/icons/buy_airtime.png';
   static const String fundAccount = 'assets/icons/fund_account.png';
 }
