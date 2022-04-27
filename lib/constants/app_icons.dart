ccdc78424e59fba8daae12a862b2d3bffd440e26
StephLak <olakulehinstephen@gmail.com>
2022-04-27 09:10:20 +0100

    Create app-icons constant

---
+++ lib/constants/app_icons.dart
@@ -0,0 +1,9 @@
+class AppIcons {
+  static const String earphone = 'assets/icons/earphone.png';
+  static const String notification = 'assets/icons/notification.png';
+  static const String copy = 'assets/icons/copy.png';
+  static const String naira = 'assets/icons/naira.png';
+  static const String avatar = 'assets/icons/avatar.png';
+  static const String arrow = 'assets/icons/arrow.png';
+  static const String fundAccount = 'assets/icons/fund_account.png';
+}
